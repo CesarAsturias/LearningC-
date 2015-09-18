bee18e13645bac1d3f3283dfd6287affedfe5f9e
César González González <supergonsi@gmail.com>
2015-09-18 16:02:33 +0200

    Modified class

--- src/prog_2.cpp
+++ src/prog_2.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
+#include <string>
 
+using std::string;
+using std::getline;
 using std::cout;
 using std::endl;
+using std::cin;
 
 // Class definition
 class Marksbook
 {
     public:
-        void showMessage()
+        void showMessage(string courseName)
         {
-            cout << "Welcome to the Marksbook!" << endl;
+            cout << "Welcome to the Marksbook for \n" << courseName << "!" 
+                << endl;
         }
 }; // Don't forget the ;
 
 
 int main()
 {
+    string nameofCourse; //String array
     Marksbook myMarksbook; // Create Marksbook object
-    myMarksbook.showMessage();
+
+    cout << "Write the course name:" << endl;
+    getline( cin, nameofCourse ); // Read the name of the course 
+    cout << endl; // Print blank line
+
+    myMarksbook.showMessage(nameofCourse);
 
     return 0;
 
