76a722fd1c602d3b7b96b55db73272daa525e6cd
César González González <supergonsi@gmail.com>
2015-09-19 21:25:43 +0200

    Modified the class

--- src/prog_2.cpp
+++ src/prog_2.cpp
@@ -11,11 +11,28 @@ using std::cin;
 class Marksbook
 {
     public:
-        void showMessage(string courseName)
+        // This function sets the course name
+        void setCourseName( string name )
         {
-            cout << "Welcome to the Marksbook for \n" << courseName << "!" 
+            courseName = name;
+        }
+
+        // This function gets the course name
+        string getCourseName()
+        {
+            return courseName;
+        }
+
+        // This function prints a welcome message
+        void showMessage()
+        {
+            cout << "Welcome to the Marksbook for \n" << getCourseName() << "!" 
                 << endl;
         }
+
+    private:
+        string courseName;
+
 }; // Don't forget the ;
 
 
@@ -23,12 +40,16 @@ int main()
 {
     string nameofCourse; //String array
     Marksbook myMarksbook; // Create Marksbook object
-
+    // Shows the initial course name
+    cout << "The initial course name is: " << myMarksbook.getCourseName() << endl;
+    // Get another course name
     cout << "Write the course name:" << endl;
-    getline( cin, nameofCourse ); // Read the name of the course 
-    cout << endl; // Print blank line
+    getline( cin, nameofCourse ); // Read the name of the course, getline read the characters of 
+                                  // the input standar object cin (keyboard), until it finds a new line
+    myMarksbook.setCourseName( nameofCourse );
 
-    myMarksbook.showMessage(nameofCourse);
+    cout << endl; // Print blank line
+    myMarksbook.showMessage();
 
     return 0;
 
